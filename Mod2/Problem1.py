92551bf01c01f2976524ddd5bbb6d8a45616ff3d
Claire <viridiansinsemilla@gmail.com>
2025-04-17 20:50:07 -0700

    Update Problem1.py

--- Mod2/Problem1.py
+++ Mod2/Problem1.py
@@ -1,4 +1,4 @@
-#problem1
+# 1. Create a program that prints the following output to the screen: "Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked."
 print("Water. Earth. Fire. Air.")
 print("Long ago, the four nations lived together in harmony.")
 print("Then, everything changed when the fire nation attacked.")
