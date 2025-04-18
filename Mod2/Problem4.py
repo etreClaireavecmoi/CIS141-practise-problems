d2bcea52a47d99928f2cb4fe7bcad81430de4ff1
Claire <viridiansinsemilla@gmail.com>
2025-04-17 20:02:06 -0700

    Add files via upload

---
+++ Mod2/Problem4.py
@@ -0,0 +1,6 @@
+year = int(input("What year were you born? (YYYY format) : "))
+age = 2025 - year
+print(f"As of this year, you are or will be {age} years old. Congratulations for making it this far!")
+
+
+#
