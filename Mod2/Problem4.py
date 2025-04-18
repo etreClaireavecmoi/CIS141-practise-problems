5ca7ba576a2349b30ebef20d4367327f2d822c75
Claire <viridiansinsemilla@gmail.com>
2025-04-17 20:49:41 -0700

    Update Problem4.py

--- Mod2/Problem4.py
+++ Mod2/Problem4.py
@@ -1,3 +1,5 @@
+# 1. Create a program that prints the following output to the screen: "Water. Earth. Fire. Air. Long ago, the four nations lived together in harmony. Then, everything changed when the Fire Nation attacked."
+
 year = int(input("What year were you born? (YYYY format) : "))
 age = 2025 - year
 print(f"As of this year, you are or will be {age} years old. Congratulations for making it this far!")
