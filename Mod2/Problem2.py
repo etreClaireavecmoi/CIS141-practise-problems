99125eceee43930d0f2216dec624f43c6684b97a
Claire <viridiansinsemilla@gmail.com>
2025-04-17 20:50:28 -0700

    Update Problem2.py

--- Mod2/Problem2.py
+++ Mod2/Problem2.py
@@ -1,4 +1,4 @@
-#problem2
+# 2. Create a program that prompts for 2 numbers and then outputs the addition, subtraction, multiplication, and division of the first number by the second number.
 x = float(input("Give me a number:"))
 y = float(input("Give me a second number:"))
 print(f"{x} + {y} = {x+y}")
