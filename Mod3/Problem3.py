ccd8dadd8825b0feaf5dbabb0061df106a1ea820
Claire <viridiansinsemilla@gmail.com>
2025-04-25 07:07:30 -0700

    Add files via upload

---
+++ Mod3/Problem3.py
@@ -0,0 +1,9 @@
+#3. Prompt the user for a sentence and a word to try to find in that sentence.
+#Have the program print out whether the word was found in the sentence. (i.e. True or False)
+
+sentence = input("Type a sentence out: ")
+word = input("Search for a word: ")
+print(word in sentence)
+
+
+
