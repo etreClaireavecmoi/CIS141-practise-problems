22ce132a95ed6b119bc595fc198be463b82df56f
Claire <viridiansinsemilla@gmail.com>
2025-05-29 21:43:01 -0700

    Update Problem1.py

--- Mod8/Problem1.py
+++ Mod8/Problem1.py
@@ -1,1 +1,8 @@
+#1. Create a file called gardening_tips.txt and add at least 3 gardening tips to
+#it. Write a Python script that reads a file gardening_tips.txt and prints
+#out each tip one by one.
 
+
+with open('gardening_tips.txt', 'r') as file:
+    for line in file:
+        print(line)
